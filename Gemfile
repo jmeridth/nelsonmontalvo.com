a9afe40d4f8028a43a52688bb534401d5ca14789
jmeridth <jmeridth@gmail.com>
2012-03-07 15:30:04 -0600

    move to static Rack app

--- Gemfile
+++
@@ -1,9 +0,0 @@
-source 'http://rubygems.org'
-
-gem 'rails', '3.0.1'
-
-gem 'mysql', '2.8.1'
-
-group :development, :test do
-  gem 'sqlite3-ruby', :require => 'sqlite3'
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Nelsonmontalvo::Application.routes.draw do
-  match '' => 'memorium#index'
-  match 'home' => 'memorium#index'
-  match 'last_picture' => 'memorium#picture'
-  match 'funeral' => 'memorium#funeral'
-  match 'blog' => 'memorium#blog'
-  match 'obituary' => 'memorium#obituary'
-  match '/:controller(/:action(/:id))'
-end
